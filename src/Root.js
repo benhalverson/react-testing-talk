a9d4c1ba11d39b9319f9e8312a9f804d76b86e0c
Ben Halverson <benhalverson33@gmail.com>
2019-06-02 21:38:45 -0700

    test(CommentList): added test for CommentList

--- src/Root.js
+++ src/Root.js
@@ -3,8 +3,8 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers';
 
-export default props => {
+export default ({ children, initalState = {} }) => {
   return (
-    <Provider store={createStore(reducers, {})}>{props.children}</Provider>
+    <Provider store={createStore(reducers, initalState)}>{children}</Provider>
   );
 };
--- src/components/__tests__/CommentList.test.js
+++ src/components/__tests__/CommentList.test.js
@@ -1,3 +1,20 @@
-it('empty test is empty', () => {
-  expect(true).toBe(true);
+import React from 'react';
+import { mount } from 'enzyme';
+import Root from '../../Root';
+import CommentList from '../CommentList';
+import '../../setupTests';
+
+let wrapped;
+beforeEach(() => {
+  const initalState = {
+    comments: ['Comment 1', 'Comment 2']
+  };
+  wrapped = mount(
+    <Root initalState={initalState}>
+      <CommentList />
+    </Root>
+  );
+});
+it('creates one LI per comment', () => {
+  expect(wrapped.find('li').length).toEqual(2);
 });
