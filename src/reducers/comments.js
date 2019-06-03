3ebb7f2856d3749385b8053cdc66d08e16e69776
Ben Halverson <benhalverson33@gmail.com>
2019-06-02 20:30:00 -0700

    test(comment reducer): Added test for reducer

--- src/reducers/comments.js
+++ src/reducers/comments.js
@@ -12,8 +12,11 @@
  * saveComment: Adds a comment via the 'comments' reducer
  */
 
+import { SAVE_COMMENT } from 'actions/types';
 export default function(state = [], action) {
   switch (action.type) {
+    case SAVE_COMMENT:
+      return [...state, action.payload];
     default:
       return state;
   }
